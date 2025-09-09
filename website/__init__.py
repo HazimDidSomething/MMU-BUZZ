2755136485efdec811ca160a6aeed77375f3db2c
Hazim Zubair <hazimzubair81@gmail.com>
2025-09-10 03:19:29 +0800

    test deploy change

--- website/__init__.py
+++ website/__init__.py
@@ -10,12 +10,14 @@ DB_name = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '[|_MMU_)(!BUZZ#)-=1?[|]'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_name}'
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg://root:TK3iZBD0rLadW6gqn4OcwlSHIDbvY4ie@dpg-d304ajndiees738v9dsg-a:5432/database_v3od"
+
     db.init_app(app)
     from .models import User, Group, GroupMember
-    CreateDatabase(app)
-    
+    with app.app_context():
+        db.create_all()
+        Createmoderator()
+
 
     from .views import views
     from .auth import auth
@@ -28,27 +30,27 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(Profile,url_prefix='/')
     app.register_blueprint(PostHandle,url_prefix='/')
-    
+
     from .models import User, Group, GroupMember
 
-    CreateDatabase(app)
+
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-   
+
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
     return app
 
-def CreateDatabase(app):
+def Createmoderator():
     from .models import User
     from werkzeug.security import generate_password_hash
-    if not path.exists('website/' + DB_name):
-        with app.app_context():
-            db.create_all()
-            print("Created Database!!!")
-            if not User.query.filter_by(email="mod@mmu.edu.my").first():
+
+    if not User.query.filter_by(email="mod@mmu.edu.my").first():
+
+
+
                 new_mod = User(
                 email="mod@mmu.edu.my",
                 FirstName="mod123",
@@ -59,5 +61,5 @@ def CreateDatabase(app):
                 db.session.add(new_mod)
                 db.session.commit()
                 print("Moderator added successfully!")
-            else:
+    else:
                 pass
