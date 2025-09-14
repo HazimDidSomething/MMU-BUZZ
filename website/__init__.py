50a82e9c664bd2f4cc0b6308eb37d545fdd38393
Hazim Zubair <hazimzubair81@gmail.com>
2025-09-15 01:44:56 +0800

    small change 4

--- website/__init__.py
+++ website/__init__.py
@@ -24,7 +24,13 @@ def create_app():
     )
     from .models import User, test, CommunityMember
     with app.app_context():
-        db.drop_all()
+    if os.getenv("RESET_DB", "false").lower() == "true":
+        with db.engine.connect() as conn:
+            conn.execute(db.text('DROP SCHEMA public CASCADE'))
+            conn.execute(db.text('CREATE SCHEMA public'))
+        db.create_all()
+        Createmoderator()
+    else:
         db.create_all()
         Createmoderator()
 
