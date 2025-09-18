f6d7b888bb78f3e7d7cf9f0ac573ec940f77f018
CHOOZHENHAO <choozhenhao07@gmail.com>
2025-09-18 12:51:50 +0800

    added community create feature

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
+
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from .models import test
-from .models import Posts,PostsImg
+from .models import Posts,PostsImg,test
+from datetime import date
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -10,7 +13,17 @@ views = Blueprint('views', __name__)
 def home():
     posts = Posts.query.order_by(Posts.date.desc()).all()
     communities = test.query.all()
+    if current_user.reset_time != date.today():
+        current_user.votes_remaining = 10
+        current_user.reset_time = date.today()
+        db.session.commit()
     return render_template("home.html", user=current_user, communities=communities,posts=posts)
 @views.route("/ping")
 def ping():
     return "pong", 200
+
+@views.route("/community")
+@login_required
+def community_page():
+    communities = test.query.all()
+    return render_template("community.html", user=current_user, communities=communities)
